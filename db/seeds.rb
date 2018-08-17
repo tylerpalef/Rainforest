056f3d73d452fb91c9f15546fb00cae506c04bcd
“Tyler <tylerpalef@gmail.com>
2018-08-17 13:16:40 -0400

    created 3 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create!(name: 'Billy Bobblehead', description: 'A bobblehead you can take home of Billy', price_in_cents: '100')
+Product.create!(name: 'David Bobblehead', description: 'A bobblehead you can take home of David', price_in_cents: '6969')
+Product.create!(name: 'Curtis Bobblehead', description: 'A bobblehead you can take home of Curtis', price_in_cents: '9001')
